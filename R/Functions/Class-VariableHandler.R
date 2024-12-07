349eea87702f0a67a984ab1fb6f289dfe7cccc58
ysdgroot <yucatan.de.groote@gmail.com>
2024-12-07 15:55:11 +0100

    Creation of class Variable handler

---
+++ R/Functions/Class-VariableHandler.R
@@ -0,0 +1,80 @@
+library(methods)
+
+
+#TODO: exclusions of interactions, if exclusion of main, don't include it 
+# setRefClass returns a generator  
+variableHandler <- setRefClass("variableHandler", 
+                      fields = list(variables = "character", 
+                                    order = "numeric", 
+                                    all_variables = "character", 
+                                    length = "numeric"))
+variableHandler$methods(
+  initialize = function(variables, order = 1){
+    # set default values
+    .self$variables <- sort(variables)
+    .self$order <- order 
+    
+    # check on the variable order
+    if(order < 1 | order%%1 != 0){
+      stop("value of m should be equal or higher than 2 and a whole number")
+    }
+    
+    # get all the combinations for each order
+    full_list <- c()
+    for (m in 1:order){
+      combinations <- combn(variables, m = m, simplify = FALSE) |> 
+        lapply(FUN = \(x)(paste(x, collapse = "*"))) |> 
+        unlist()
+      
+      full_list <- c(full_list, combinations)
+    }
+    
+    .self$all_variables <- full_list
+    .self$length <- length(full_list)
+  }, 
+  getVariables = function(coding){
+    
+    if(length(coding) != .self$length){
+      stop(sprintf("Length of the coding is incorrect, it should have length %d", .self$length))
+    }
+    
+    if(!all(coding %in% c(0,1))){
+      stop("It should only contain 0 and 1")
+    }
+    return(.self$all_variables[coding == 1])
+  }, 
+  getCoding = function(variables, withMain = FALSE){
+    
+    if(!all(variables %in% .self$all_variables)){
+      stop("Not all given variables exists")
+    }
+    
+    variables_select <- unique(variables)
+    
+    if(withMain){
+      main_variables <- strsplit(variables_select, "\\*" )  |> 
+        unlist() |> 
+        unique()
+      
+      variables_select <- unique(c(main_variables, variables_select))
+    }
+    
+    coding <- rep(0, .self$length)
+    positions <- which(.self$all_variables %in% variables_select)
+    coding[positions] <- 1
+    
+    return(coding)
+  } 
+)
+
+
+# -------------------------------------------------------------------------
+
+
+test_vh <- variableHandler$new(variables = c("Age", "Region", "Gender"), order = 2)
+
+test_vh$getCoding(c("Region", "Age*Region"), withMain = TRUE)
+test_vh$getCoding(c("Region", "Age*Region"), withMain = FALSE)
+
+
+
