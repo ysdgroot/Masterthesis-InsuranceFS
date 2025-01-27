d7a13e88ef74290fd83e7a5c41091b42c1b3cec7
ysdgroot <yucatan.de.groote@gmail.com>
2025-01-27 21:05:01 +0100

    update code

--- R/FS-GLM-GA.R
+++ R/FS-GLM-GA.R
@@ -25,14 +25,15 @@ sapply(list.files(file.path("R", "GA"),
 
 ## Parameters --------------------------------------------------------------
 
-mfitness <- memoise::memoise(concProb_glm_bin)
+mfitness <- memoise::memoise(concProb_glm_fastglm)
 
 # random suggestion sample 
 suggestions <- matrix(as.double(NA), 
-                      nrow = 10, 
+                      nrow = 9, 
                       ncol = VH$getLength())
-for(j in 1:10) { positions <- sample.int(n = VH$getLength(), 
-                                         size = 20)
+for(j in 1:9) { 
+positions <- sample.int(n = VH$getLength(), 
+                        size = 5)
 suggestion <- rep(0,  VH$getLength())
 suggestion[positions] <- 1
 
@@ -42,21 +43,24 @@ suggestions[j,] <- suggestion
 suggestions <- rbind(VH$getCoding(vars), 
                      suggestions)
 
-## Running function --------------------------------------------------------
 
-GA_sim_1 <- ga(fitness = concProb_glm_bin, 
-             trainDT = trainDT, 
-             testDT = testDT, 
-             distMod = poisson(link='log'), 
-             offset = "exposure", 
-             targetVar = "claimNumber", 
-             variableHandler = VH, 
-             nullValue = 0.5,
-             maxVar = 30, 
-             withMain = TRUE,
+
+## Running function --------------------------------------------------------
+#TODO: problem with the GA, if the suggestions are bigger than the population size, it will cause an error
+
+GA_sim_1 <- ga(fitness = mfitness, 
+                  trainDT = trainDT, 
+                  testDT = testDT, 
+                  variableHandler = VH, 
+                  targetVar = "claimNumber", 
+                  distMod = poisson(link='log'), 
+                  offset = "exposure", 
+                  nullValue = 0.5,
+                  withMain = TRUE,
+                  location_save = full_folder_name,
              type = "binary", # optimization data type
              population = gabin_Population,
-             suggestions = VH$getCoding(vars), # 
+             suggestions = suggestions, 
              crossover = gabin_uCrossover,  # cross-over method
              selection = gabin_rwSelection, # roulette selection method
              mutation = gabin_raMutation, 
@@ -64,10 +68,10 @@ GA_sim_1 <- ga(fitness = concProb_glm_bin,
              pmutation = 0.2, # mutation rate prob
              popSize = 10, # number of the population in each evolution
              nBits = VH$getLength(), # total number of variables
-             names= VH$getUsedVariables(), # variable name
-             run=10, # max iter without improvement (stopping criteria)
+             names = VH$getUsedVariables(), # variable name
+             run = 10, # max iter without improvement (stopping criteria)
              maxiter = 50, # total runs or generations
-             monitor=plot, # plot the result at each iteration
+             monitor = plot, # plot the result at each iteration
              keepBest = TRUE, # keep the best solution at the end
              parallel = TRUE, # allow parallel processing
              seed=84211 # for reproducibility purposes
