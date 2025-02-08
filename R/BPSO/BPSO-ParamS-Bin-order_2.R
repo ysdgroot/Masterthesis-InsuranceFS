398f464b48edeeaba333dc39d96fe6fcc972d1fa
ysdgroot <yucatan.de.groote@gmail.com>
2025-02-08 16:12:59 +0100

    Add scripts for order 2 testing

---
+++ R/BPSO/BPSO-ParamS-Bin-order_2.R
@@ -0,0 +1,126 @@
+# Importing library -------------------------------------------------------
+
+source(file.path("R", "Packages.R"))
+
+source(file.path("R", "General Parameters.R"))
+source(file.path("R", "Config-order2.R"))
+
+# Parameter selection is based on the best results of order 1
+# otherwise to much compute time 
+
+stop("Not Implemented Yet")
+
+# Parameter Selection  ----------------------------------------------------
+# Best selection by Best found and Least number of iterations 
+k1 <- seq(1, 6, 0.5) 
+k2 <- seq(1, 6, 0.5)
+w <- seq(0.1, 2.1, 0.5)
+pop_size <- seq(10, 25, 5)
+
+base_tests <- expand.grid("k1" = k1, 
+                          "k2" = k2, 
+                          "w" = w, 
+                          "TransFun" = baseClassTransferFunctions, 
+                          "Popsize" = pop_size)
+setDT(base_tests)
+base_tests[, ID := .I]
+
+# save all the results
+list_results <- list()
+
+# base parameters 
+max_iter <- 30 
+max_stable <- 10
+
+total_runs <- nrow(base_tests)
+
+for (ifold in 1:nfolds) {
+  cat(crayon::blue(sprintf("Start Fold: %d ---------------- \n", 
+                           ifold)))
+  
+  trainDT_fold <- inputDT[Fold != ifold]
+  testDT_fold <- inputDT[Fold == ifold]
+  
+  # create folder to save results 
+  folder_name <- sprintf(base_name_folder, 
+                         seed,
+                         ifold, 
+                         nfolds, 
+                         "bin", 
+                         VH$get_length(), 
+                         null_value)
+  full_folder_name <- file.path("Data", folder_name)
+  dir.create(full_folder_name, showWarnings = FALSE)
+  
+  for (i in 1:nrow(base_tests)) {
+    cat(crayon::blue(sprintf("Fold: %d \t Run: %d/%d \n", 
+                             ifold, 
+                             i, 
+                             total_runs)))
+    
+    BPSO_gen <- BPG_Velocity$new(ParticleBPSO, 
+                                 chance_bit = 0.2,
+                                 suggestions = NULL)
+    BPSO_swarm <- SwarmBPSO$new(base_tests[i,]$Popsize, 
+                                VH$get_length(), 
+                                transferFun = base_tests[i,][["TransFun"]][[1]], 
+                                BPSO_gen, 
+                                w = base_tests[i,]$w, 
+                                k1 = base_tests[i,]$k1, 
+                                k2 = base_tests[i,]$k2, 
+                                seed = 420)
+    
+    BPSO_run <- BPSO_swarm$run_process(concProb_glm_fastglm, 
+                                       max_stable = max_stable, 
+                                       max_iter = max_iter,
+                                       args_fun = list(
+                                         trainDT = trainDT_fold, 
+                                         testDT = testDT_fold, 
+                                         distMod = poisson(link = "log"), 
+                                         variableHandler = VH, 
+                                         nullValue = null_value, 
+                                         type = "bin", 
+                                         offset = "exposure", 
+                                         targetVar = "claimNumber", 
+                                         location_save = full_folder_name
+                                       ), 
+                                       seed = 123)
+    
+    # save the results 
+    
+    result <- data.table("nIterations" = BPSO_swarm$get_iteration(), 
+                   "BestResult" = BPSO_run$BestResult$Result, 
+                   "ID" = i, 
+                   "Fold" = ifold)
+    cat(green(sprintf("ID: %d - nIterations: %d - Result: %g - Position: %s \n", 
+                      i, 
+                      BPSO_swarm$get_iteration(), 
+                      BPSO_run$BestResult$Result, 
+                      paste(BPSO_run$BestResult$Position, collapse = ""))))
+    
+    list_results <- append(list_results, list(result))
+  }
+}
+
+dt_results_cv_bpso <- rbindlist(list_results)
+
+dir.create(file.path("Data", 
+                     "Parameters"), 
+           showWarnings = FALSE)
+
+#Use the name of the Transfer function, rather than the object
+base_tests_temp <- copy(base_tests)
+base_tests_temp[, TransFunName := as.character(lapply(TransFun, \(x) x$get_name()))]
+base_tests_temp[, TransFun := NULL]
+
+dt_results_cv_bpso <- dt_results_cv_bpso |> 
+  collapse::join(base_tests_temp, 
+                 on = 'ID', 
+                 how = "left")
+
+saveRDS(dt_results_cv_bpso, 
+        sprintf(file.path("Data", 
+                          "Parameters", 
+                          "Param_BPSO_bin_order%d.RDS"), 
+                order))
+
---
+++ R/GA/GA-ParamS-Bin-order_2.R
@@ -0,0 +1,152 @@
+# Importing library -------------------------------------------------------
+
+source(file.path("R", "Packages.R"))
+
+source(file.path("R", "General Parameters.R"))
+source(file.path("R", "Config-order2.R"))
+
+# Parameter selection is based on the best results of order 1
+# otherwise to much compute time 
+
+stop("Not Implemented Yet")
+
+# Parameter Selection  ----------------------------------------------------
+# number of elits 
+# population size 
+####### selection
+# Linear-rank selection -- r and q
+# Nonlinear-rank selection -- q
+# roulette wheel --
+# Tournament Selection -- k = 3
+
+####### crossover
+# Uniform crossover 
+
+####### Mutation 
+# Uniform-random --> probability
+
+n_elits <- 2:5
+pop_size <- seq(10, 25, 5)
+
+p_crossover <- seq(0.5, 0.9, 0.1)
+p_mutation <- seq(0.1, 0.5, 0.1)
+
+name_selection <- c("linear", "nonlinear", "roulette", "tournament")
+dt_selection <- data.table(NameSelection = name_selection, 
+                           Selection = c(gabin_lrSelection, 
+                                         gabin_nlrSelection, 
+                                         gabin_rwSelection, 
+                                         gabin_tourSelection))
+
+base_tests <- expand.grid("n_elits" = n_elits, 
+                          "pop_size" = pop_size, 
+                          "p_crossover" = p_crossover, 
+                          "p_mutation" = p_mutation, 
+                          "NameSelection" = name_selection)
+setDT(base_tests)
+
+base_tests <- base_tests |> 
+  collapse::join(dt_selection, 
+                 on = "NameSelection", 
+                 how = "left")
+base_tests[, ID := .I]
+
+# save all the results
+list_results <- list()
+
+max_iter <- 30 
+max_stable <- 10
+
+total_runs <- nrow(base_tests)
+
+for (ifold in 1:nfolds) {
+  cat(crayon::blue(sprintf("Start Fold: %d ---------------- \n", 
+                           ifold)))
+  
+  trainDT_fold <- inputDT[Fold != ifold]
+  testDT_fold <- inputDT[Fold == ifold]
+  
+  # create folder to save results 
+  folder_name <- sprintf(base_name_folder, 
+                         seed,
+                         ifold, 
+                         nfolds, 
+                         "bin", 
+                         VH$get_length(), 
+                         null_value)
+  full_folder_name <- file.path("Data", folder_name)
+  dir.create(full_folder_name, showWarnings = FALSE)
+  
+  for (i in 1:nrow(base_tests)) {
+    cat(crayon::blue(sprintf("Fold: %d \t Run: %d/%d \n", 
+                             ifold, 
+                             i, 
+                             total_runs)))
+    
+    GA_sim <- ga(fitness = mfitness, 
+                       trainDT = trainDT, 
+                       testDT = testDT, 
+                       variableHandler = VH, 
+                       targetVar = "claimNumber", 
+                       distMod = poisson(link='log'), 
+                       offset = "exposure", 
+                       nullValue = 0.5,
+                       withMain = TRUE,
+                       location_save = full_folder_name,
+                   type = "binary", # optimization data type
+                   population = gabin_Population,
+                   suggestions = NULL,
+                   selection = base_tests[i,]$Selection[[1]], 
+                   crossover = gabin_uCrossover,  # cross-over method
+                   pcrossover = base_tests[i,]$p_crossover,
+                   mutation = gabin_raMutation, # uniform random 
+                   pmutation = base_tests[i,]$p_mutation, 
+                   elitism = base_tests[i,]$n_elits, # best N indiv. to pass to next iteration
+                   popSize = base_tests[i,]$pop_size, # number of the population in each evolution
+                   nBits = VH$get_length(), # total number of variables
+                   names = VH$get_used_variables(), # variable name
+                   run = max_stable, # max iter without improvement (stopping criteria)
+                   maxiter = max_iter, # total runs or generations
+                   monitor = FALSE, 
+                   keepBest = TRUE, # keep the best solution at the end
+                   parallel = FALSE, # don't do parallel because it takes to much time 
+                   seed=9876)
+    
+    
+    # save the results 
+    
+    result <- data.table("nIterations" = GA_sim@iter, 
+                         "BestResult" = GA_sim@fitnessValue, 
+                         "ID" = i, 
+                         "Fold" = ifold)
+    cat(green(sprintf("ID: %d - nIterations: %d - Result: %g - Position: %s \n", 
+                      i, 
+                      GA_sim@iter, 
+                      GA_sim@fitnessValue, 
+                      paste(GA_sim@bestSol[[length(GA_sim@bestSol)]], collapse = ""))))
+    
+    list_results <- append(list_results, list(result))
+  }
+}
+
+dt_results_cv_ga <- rbindlist(list_results)
+
+dir.create(file.path("Data", 
+                     "Parameters"), 
+           showWarnings = FALSE)
+
+#Use the name of the Transfer function, rather than the object
+base_tests_temp <- copy(base_tests)
+base_tests_temp[, Selection := NULL]
+
+dt_results_cv_ga <- dt_results_cv_ga |> 
+  collapse::join(base_tests_temp, 
+                 on = 'ID', 
+                 how = "left")
+
+saveRDS(dt_results_cv_ga, 
+        sprintf(file.path("Data", 
+                          "Parameters", 
+                          "Param_GA_bin_order%d.RDS"), 
+                order))
+
