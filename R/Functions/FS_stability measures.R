27228c701d8584114bebf1340cf2d743d5e59d8a
ysdgroot <yucatan.de.groote@gmail.com>
2025-03-17 17:26:03 +0100

    Add functions
    For stability measures of feature sets
    
    conversion of the results of the GA into data.table

---
+++ R/Functions/FS_stability measures.R
@@ -0,0 +1,45 @@
+#' Jaccard index for feature selection
+#'
+#' @param s1,s2 binary vectors of the same length
+#'
+#' @returns value between 0 and 1
+#' @export
+jaccard_index <- function(s1, s2) {
+  sum(s1*s2)/sum((s1+s2) > 0)
+}
+
+#' Pearson Correlation for feature selection
+#'
+#' @inheritParams jaccard_index
+#'
+#' @returns correlation of 2 variables
+#' @export
+pearson_cor <- function(s1, s2) {
+  cor(s1, s2, method = "pearson")
+}
+
+#' Average Pairwise Similarity measure
+#'
+#' @param feature_sets list with binary vectors
+#' @param measure function which takes 2 binary vectors as input
+#'
+#' @returns numeric
+#' @export
+similarity_measure <- function(feature_sets, 
+                               measure = pearson_cor) {
+  
+  # number of sets
+  M <- length(feature_sets)
+  
+  total_measures <- 0
+  for (i in 1:M) {
+    for (j in 1:M) {
+      if(i == j) {break}
+      
+      total_measures <- total_measures  +
+        measure(feature_sets[[i]], feature_sets[[j]])
+    }
+  }
+  result <- total_measures /(M * (M-1))
+  return(result)
+}
